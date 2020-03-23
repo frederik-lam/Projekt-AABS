ddc596871cc2aee3448284167f693c97b5dbd198
jakubmajercik <jakub.majercik@gmail.com>
2020-03-23 11:38:06 +0100

    zatial tam nic nie je

---
+++ MyMethod.m
@@ -0,0 +1,13 @@
+function output = MyMethod( data )
+%Do této funkce implementujte váši metody pro pøedzpracování dat a
+%veškeré algoritmy pro rozpoznávání fibrilace síní v EKG.
+
+%Vstup:             
+%data: vstupní surová data reprezentující 1 objekt (EKG záznam jednoho pacienta). 
+
+%Výstup:            
+%output: zaøazení objektu do jedné z kategorii (1-fibrilace síní, 0-ostatní)
+
+output = [];
+
+end
